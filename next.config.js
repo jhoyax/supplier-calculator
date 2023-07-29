dcb35b7ece01dc8330cfe424598af343614027ed
Joy <joylopenaasis@gmail.com>
2023-07-29 22:21:30 +0800

    Adjust config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    assetPrefix: '/supplier-calculator/',
+}
 
 module.exports = nextConfig
