8712299bb09dbe5c67d98cc1b35b8b31df0a545d
Joy <joylopenaasis@gmail.com>
2023-07-29 22:46:09 +0800

    Update base path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: '/supplier-calculator/',
+    basePath: isProd ? '/supplier-calculator' : undefined,
+    assetPrefix: isProd ? '/supplier-calculator/' : undefined, // assetPrefix requires the trailing slash
 }
 
 module.exports = nextConfig
