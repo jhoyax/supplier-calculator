6d5c6fd18aa4a34980b80a2cf5e0e69e5742c1a5
Joy <joylopenaasis@gmail.com>
2023-07-29 23:09:22 +0800

    Set trailingSlash to true

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    trailingSlash: true,
     basePath: isProd ? '/supplier-calculator' : undefined,
     assetPrefix: isProd ? '/supplier-calculator/' : undefined, // assetPrefix requires the trailing slash
 }
